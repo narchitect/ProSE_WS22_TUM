196188bc15cb2aa2a3c7c3bec72d42bd70b6eeba
Nayun Kim <narchitect@icloud.com>
2022-11-06 21:28:21 +0100

    operations finshed
    creating operations method including union, difference, intersection

--- Assignment/Intersection.cs
+++ Assignment/Intersection.cs
@@ -1,12 +1,12 @@
 using System;
 
 namespace Assignment{
-    public class Difference : Operation
+    public class Intersection : Operation
     {
         //추상 클래스의 자식 클래스는 추상부모클래스의 생성자를 이용해한다.
-        public Difference(ImplicitGeometry operand1, ImplicitGeometry operand2 ) : base(operand1, operand2) {}
+        public Intersection(ImplicitGeometry operand1, ImplicitGeometry operand2 ) : base(operand1, operand2) {}
         public override bool IsInside(double x, double y) {
-            return operand1.IsInside(x,y) && !operand2.IsInside(x,y);
+            return operand1.IsInside(x,y) && operand2.IsInside(x,y);
         }
     
     }
