5960d026dbb6b05f0db81567bafc69696135e2cb
Nayun Kim <ny.kim@tum.de>
2022-11-17 08:40:01 +0100

    test

---
+++ Lec03_Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lec03_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
