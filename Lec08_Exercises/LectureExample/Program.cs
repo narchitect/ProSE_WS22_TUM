3e445ba3929e2ea9b1c3bb59399dfe514ea445e1
Nayun Kim <ge37kan@mytum.de>
2023-01-08 16:34:57 +0100

    Lecture08_Examples_ExtensionMethods
    to explain Linq, it explained different ways to create a select certain data in a dataset.
    
    1. the old way using statements directly inside the class.
    2. by using a method to filter the data
    3. by using a delegate(Func)
    4. by using a Linq

---
+++ Lec08_Exercises/LectureExample/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+namespace Program
+
+{
+    public class People
+    {
+        public int Age { get; set; }
+        public decimal Income { get; set; }
+    }
+
+
+    // ---------03. Using Extension Methods
+    // * ExtensionClass should be defined in the top level static class in a namespace. 
+    static public class ExtensionClass
+    {
+        // 여기 Func generic delegate를 쓴 이유
+        // fillterPeople method는 dataset 과 어떤 method를 받는데, 어떠한 메소드는 People 클래스를 parameter, return 값은 bool로 받음
+        static public List<People> FilterPeople(this List<People> dataset, Func<People, bool> filterFunction)
+        {
+            var bucket = new List<People>();
+            foreach (var p in dataset)
+            {
+                // 그 메소드가 p라는 인스턴스를 받아드리고 그 값이 bool이기때문에 
+                if (filterFunction(p))
+                {
+                    bucket.Add(p);
+                }
+            }
+            return bucket;
+        }
+    }
+
+
+    internal class Program
+    {
+        public static bool ReturnBool(People par1)
+        {
+            if (par1.Age > 30)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main01(string[] arg)
+        {
+
+            var dataset = new List<People>
+            {
+                new People {Age = 45, Income = 35000},
+                new People {Age = 15, Income = 45000},
+                new People {Age = 35, Income = 25000},
+                new People {Age = 55, Income = 15000},
+                new People {Age = 5, Income = 5000},
+                new People {Age = 25, Income = 135000}
+            };
+
+            //-------------03 using ExtensionMethod
+            // 여기 람다식에서는 데이터타입을 지정할 필요가 없는데 이유는 Func에서 이미 데이터 형식을 지정해줬기때문에 참조 가
+            var bucket = dataset.FilterPeople(people => people.Age > 30);
+
+            //-------------01 old way to create a database
+            //var bucket = new List<People>();
+            //foreach (var p in dataset)
+            //{
+            //    if (p.Age > 30)
+            //    {
+            //        bucket.Add(p);
+            //    }    
+            //};
+            //    foreach (var p in bucket)
+            //    {
+            //        Console.WriteLine("The age is " + p.Age.ToString());
+            //    }
+            //}
+
+            //-------------02 create a method to filter dataset
+            //static List<People> FilterPeople (List<People> dataset)
+            //{
+            //    var bucket = new List<People>();
+            //    foreach (var p in dataset)
+            //    {
+            //        if (p.Age > 30)
+            //        {
+            //            bucket.Add(p);
+            //        }
+            //    }
+            //    return bucket;
+            //}
+            //    var bucket = FilterPeople(dataset);
+            //    foreach (var p in bucket)
+            //    {
+            //        Console.WriteLine("The age is " + p.Age.ToString());
+            //    }
+            //}
+
+            //--------------04. Using Linq
+
+            var bucket02 = dataset.Where(p => p.Age > 30).Where(p => p.Income > 50000);
+        }
+    }
+}
---
+++ Lec08_Exercises/LectureExample/Program02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LectureExample
+{
+    public class SinglePurposeThing
+    {
+        public int SomeValue { get; set;}
+    }
+
+    //02. then create an extenstionMethod class
+    public static class ExtensionMethods
+    {
+        //when you use this keyword, it allows to the method "Set2Five" of the class "ExtensionMethods" bind to the class "SinglePurposeThing")
+        public static void Set2Five(this SinglePurposeThing arg)
+        {
+            arg.SomeValue = 5;
+        }
+    }
+    internal class Program02
+    {
+        //static void Set2Five(SinglePurposeThing sps)
+        //{
+        //    sps.SomeValue = 5;
+        //}
+
+        static void Main(string[] args)
+        {
+            var sps = new SinglePurposeThing();
+            //Set2Five(sps);
+            //01. we can't access the method "Set2Five" from the object "sps" but we don't want to define the method inside the class.
+
+            //03. now you can call the method "Set2Five" with an instance
+
+            sps.Set2Five();
+
+            Console.WriteLine(sps.SomeValue.ToString()) ;
+        }
+    }
+}
+
